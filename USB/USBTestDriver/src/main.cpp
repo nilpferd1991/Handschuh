c5f8754c9427ebcc4c1b27fd996cf162d22d58bf
Nils <area51.nils@googlemail.com>
2014-01-07 21:36:01 +0100

    time measurement inserted and display stuff deleted

--- USB/USBTestDriver/src/main.cpp
+++ USB/USBTestDriver/src/main.cpp
@@ -12,131 +12,7 @@
 #include "certificate.h"
 #include "usb.h"
 
-#include <ncurses.h>
-
-int counter = 0;
-int status = 0;
-
-#define STATUS_STARTING 0
-#define STATUS_STEADY_STATE 1
-#define STATUS_AFTER_STEADY_STATE 2
-#define STATUS_NOT_STEADY_STATE 3
-#define STATUS_BEFORE_STEADY_STATE 4
-
-
-#define min(x,y) (x > y ? y : x)
-#define max(x,y) (x < y ? y : x)
-
-void setStatus(int_least16_t x, int_least16_t y, int_least16_t z) {
-
-	// Daten zeichnen
-	move( 10, 0);
-	clrtoeol();
-	printw("%d %d %d", x, y, z);
-
-	move( 11, 0); clrtoeol();
-	move( 12, 0); clrtoeol();
-	move( 13, 0); clrtoeol();
-
-	int maxx = getmaxx(stdscr);
-	int maxy = getmaxy(stdscr);
-
-	for(int i = 0; i < maxx; i++) {
-		if(i > min(maxx/2, x/10 + maxx/2) && i < max(maxx/2, x/10 + maxx/2)) {
-			move( 11, i);
-			addch('x');
-		}
-		if(i > min(maxx/2, y/10 + maxx/2) && i < max(maxx/2, y/10 + maxx/2)) {
-			move( 12, i);
-			addch('y');
-		}
-		if(i > min(maxx/2, z/10 + maxx/2) && i < max(maxx/2, z/10 + maxx/2)) {
-			move( 13, i);
-			addch('z');
-		}
-	}
-
-	// Schrift ausgeben
-
-	move(4,0);
-	printw("state zero: false\n");
-
-	move(3,0);
-	switch(status) {
-	// Gerade gestartet
-	case STATUS_STARTING:
-		move(4,0);
-		printw("state zero: maybe\n");
-		// Sind wir im Steady-State?
-		if(x*x < STEADY_STATE_THRESHOLD && y*y < STEADY_STATE_THRESHOLD &&
-				z*z < STEADY_STATE_THRESHOLD) {
-			status = STATUS_STEADY_STATE;
-			counter = 0;
-		}
-		break;
-	// Gerade im Steady-State
-	case STATUS_STEADY_STATE:
-		move(4,0);
-		printw("state zero: maybe\n");
-
-		if(x*x < STEADY_STATE_THRESHOLD && y*y < STEADY_STATE_THRESHOLD &&
-				z*z < STEADY_STATE_THRESHOLD) {
-			counter++;
-		}
-
-		// Sind wir lange genug im Steady-State geblieben?
-		if(counter > STEADY_STATE_CYCLES) {
-			status = STATUS_AFTER_STEADY_STATE;
-			counter = 0;
-		}
-		break;
-	// Nach dem Steady-State
-	case STATUS_AFTER_STEADY_STATE:
-		move(4,0);
-		printw("state zero: true\n");
-
-		if(!(x*x < STEADY_STATE_THRESHOLD && y*y < STEADY_STATE_THRESHOLD &&
-				z*z < STEADY_STATE_THRESHOLD)) {
-			status = STATUS_NOT_STEADY_STATE;
-		}
-		break;
-	// Nach Steady-State mit Bewegung
-	case STATUS_NOT_STEADY_STATE:
-		move(4,0);
-		printw("state zero: false\n");
-
-		if(x*x < STEADY_STATE_THRESHOLD && y*y < STEADY_STATE_THRESHOLD &&
-				z*z < STEADY_STATE_THRESHOLD) {
-			status = STATUS_BEFORE_STEADY_STATE;
-			counter = 0;
-		}
-		break;
-	case STATUS_BEFORE_STEADY_STATE:
-
-		move(4,0);
-		printw("state zero: maybe\n");
-
-		if(x*x < STEADY_STATE_THRESHOLD && y*y < STEADY_STATE_THRESHOLD &&
-				z*z < STEADY_STATE_THRESHOLD) {
-			counter++;
-		}
-		else {
-			status = STATUS_NOT_STEADY_STATE;
-			break;
-		}
-
-		// Sind wir lange genug im Steady-State geblieben?
-		if(counter > STEADY_STATE_CYCLES) {
-			status = STATUS_AFTER_STEADY_STATE;
-			counter = 0;
-		}
-		break;
-	default:
-		break;
-	}
-
-	refresh();
-}
+#include <sys/time.h>
 
 int main(int argc, char **argv)
 {
@@ -149,51 +25,43 @@ int main(int argc, char **argv)
 	// Data-Test
 	buffer = new uint8_t[CUSTOM_RQ_DATA_LEN];
 
-	// Ncurses - Init
-	initscr();
-	noecho();	// Die Eingaben nicht ausgeben
-	raw();		// Alle Eingaben abfangen
-	nodelay(stdscr, TRUE); // Nicht auf Eingaben warten
-	printw("Welcome to the control programm for the handglove!\n");
-
-	// Startnachricht
-	printw("Please go to state zero.\n");
-	refresh();
-
-	int key;
+	struct timeval begin, end;
+	long seconds, useconds;
 
 	for(;;) {
 
-		// User hat eine Taste gedrückt
-		if ((key = getch()) != ERR) {
-			usbClose();
-
-			endwin();
-			return 0;
-		}
 
 		// Daten abrufen
-		int_least16_t x,y,z;
 
-		for(int i = 0; i < MEASURE_CYCLES; i++) {
-			usbRead(CUSTOM_RQ_DATA, 0, buffer, CUSTOM_RQ_DATA_LEN);
+		if (gettimeofday(&begin,(struct timezone *)0)) {
+			fprintf(stderr, "can not get time\n");
+		    exit(1);
+		}
 
-			x += ((int_least8_t)buffer[1] << 2 | (int_least8_t)(buffer[0] >> 6)) - OFFSET_X;
-			y += ((int_least8_t)buffer[3] << 2 | (int_least8_t)(buffer[2] >> 6)) - OFFSET_Y;
-			z += ((int_least8_t)buffer[5] << 2 | (int_least8_t)(buffer[4] >> 6)) - OFFSET_Z;
+		for(int i = 0; i < 1000; i++) {
+			usbRead(CUSTOM_RQ_DATA, 0, buffer, 16);
 
 		}
 
-		x /= MEASURE_CYCLES;
-		y /= MEASURE_CYCLES;
-		z /= MEASURE_CYCLES;
+		if (gettimeofday(&end,(struct timezone *)0)) {
+			fprintf(stderr, "can not get time\n");
+			exit(1);
+		}
 
-		setStatus(x,y,z);
+	  printf("begin:\t\t\t\t%ld sec %ld usec\n", begin.tv_sec, begin.tv_usec);
+	  printf("end:\t\t\t\t%ld sec %ld usec\n", end.tv_sec, end.tv_usec);
+	  seconds = end.tv_sec - begin.tv_sec;
+	  useconds = end.tv_usec - begin.tv_usec;
+	  if(useconds < 0) {
+			useconds += 1000000;
+			seconds--;
+	  }
 
+	  printf("Dauer eines Abrufs:\t\t%ld usec\n", (seconds*1000000L +  useconds)/1000L);
+	  printf("Abrufe pro Sekunde:\t\t%lg\n\n", 1000000/(double)((seconds*1000000L +  useconds)/1000L));
 	}
 
 	usbClose();
 
-	endwin();
 	return 0;
 }
